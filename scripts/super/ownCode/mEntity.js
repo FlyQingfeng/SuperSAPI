18d47cb765e40190db3c993c35a4cf466db2f479
FlyQingfeng <2758534648@qq.com>
2024-10-17 12:29:40 +0800

    初始化网页

--- scripts/super/ownCode/mEntity.js
+++
@@ -1,9 +0,0 @@
-import * as SuperSAPI from "../SuperSAPI";
-export class mEntity extends SuperSAPI.Entity {
-    constructor(entity, world) {
-        super(entity, world);
-    }
-    onDieAfterEvent(event) {
-        console.log("onDieAfterEvent");
-    }
-}
